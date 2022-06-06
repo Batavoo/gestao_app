5034e3d44ff606199b21e9d4e2bf36761f0a6f71
gustavo <gsarruda.office@gmail.com>
2022-06-06 16:43:30 -0300

    added lateinit var cringers

--- app/src/main/java/com/example/myapplication/Login.kt
+++ app/src/main/java/com/example/myapplication/Login.kt
@@ -2,8 +2,12 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class Login : AppCompatActivity() {
+
+    private lateinit var cringers : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
