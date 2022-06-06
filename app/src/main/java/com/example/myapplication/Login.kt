8ffd2dfa26674fbc7c89a71d01cb9c8069f062cc
gustavo <gsarruda.office@gmail.com>
2022-06-06 16:52:33 -0300

    test

--- app/src/main/java/com/example/myapplication/Login.kt
+++ app/src/main/java/com/example/myapplication/Login.kt
@@ -5,9 +5,6 @@ import android.os.Bundle
 import android.widget.EditText
 
 class Login : AppCompatActivity() {
-
-    private lateinit var cringers : EditText
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
