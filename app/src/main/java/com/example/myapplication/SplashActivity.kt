b00039161c28bfb7f9ef6a2162556e83b9bbe569
gustavo <gsarruda.office@gmail.com>
2022-06-08 06:32:53 -0300

    modificações no manifesto

--- app/src/main/java/com/example/myapplication/SplashActivity.kt
+++ app/src/main/java/com/example/myapplication/SplashActivity.kt
@@ -2,10 +2,21 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.content.Intent
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val handler = Handler(Looper.myLooper()!!)
+
+        handler.postDelayed({
+            val it = Intent(this,Login::class.java)
+            startActivity(it)
+            finish()
+        },3000L)
     }
 }
